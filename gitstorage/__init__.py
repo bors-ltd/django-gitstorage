0dcafe990c0cbae7731027c2fc439969fbd77f38
Hervé Cauwelier <herve@bors-ltd.fr>
2015-12-31 12:55:49 +0100

    stop lying about the maturity of this library

--- gitstorage/__init__.py
+++ gitstorage/__init__.py
@@ -7,5 +7,5 @@ __contact__ = "gitstorage@bors-ltd.fr"
 __homepage__ = "https://github.com/bors-ltd/django-gitstorage"
 __license__ = "GPL3"
 
-__version__ = "1.1.0"
+__version__ = "0.1.0"
 VERSION = tuple(__version__.split("."))
