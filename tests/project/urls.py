3b4c4c81bc5d3d8894e399a2f5ca59c21e3ce310
Hervé Cauwelier <herve@bors-ltd.fr>
2019-07-22 15:08:07 +0200

    cleanup

--- tests/project/urls.py
+++ tests/project/urls.py
@@ -1,18 +1,19 @@
-from django.conf.urls import url
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     # Blob views
-    url(r"^(?P<path>.+)/;inline", views.TestInlineView.as_view(), name="blob_inline"),
-    url(
-        r"^(?P<path>.+)/;download$",
-        views.TestDownloadView.as_view(),
-        name="blob_download",
+    path("<path:path>/;inline", views.TestInlineView.as_view(), name="blob_inline"),
+    path(
+        "<path:path>/;download", views.TestDownloadView.as_view(), name="blob_download"
     ),
     # Tree views (including the root)
-    url(r"^(?P<path>.*)/?;shares$", views.TestSharesView.as_view(), name="tree_shares"),
-    url(r"^(?P<path>.*)/?;share$", views.TestShareView.as_view(), name="tree_share"),
+    # Don't use the path converter, the empty string is a valid path
+    re_path(
+        r"^(?P<path>.*)/;shares$", views.TestSharesView.as_view(), name="tree_shares"
+    ),
+    re_path(r"^(?P<path>.*)/;share$", views.TestShareView.as_view(), name="tree_share"),
     # Browse/catch-all view
-    url(r"^(?P<path>.*)$", views.TestRepositoryView.as_view(), name="repo_browse"),
+    re_path(r"^(?P<path>.*)$", views.TestRepositoryView.as_view(), name="repo_browse"),
 ]
